60d8a5aaf37bc99e1c3db9935208097cccda5893
Fionn-Lin <3176495097@qq.com>
2022-10-26 19:33:47 +0800

    Add  double h = 0.001;         double x = 0;         double y[10000];         double a;         printf(请输入a=);         scanf_s(%lf, &a);         y[0] = a;         for (int i = 1; i < 10000

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
-	return 0;
-}
+int main(int argc, char *argv[]) 
+    {
+        double h = 0.001;
+        double x = 0;
+        double y[10000];
+        double a;
+        printf("请输入a=");
+        scanf_s("%lf", &a);
+        y[0] = a;
+        for (int i = 1; i < 10000; i++) 
+        {
+            x = x + h;
+            y[i] = y[i - 1] + -h * y[i - 1];
+            printf("x=%.8lf     y=%.8lf\v", x, y[i]);
+            double e = y[i - 1] - y[i];
+            while (e <= 0.001 * h)
+            {
+                printf("定态丁真，鉴定为" "%.6lf", y[i]);
+                return 0;
+            }
+        }
+
+
+       
+    }
+	
+
 
